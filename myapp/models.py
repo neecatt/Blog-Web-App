dd85bcd74fb73e301efe318e3eaf8d0bce122073
HorrorOTB <nicatabdullazade135@gmail.com>
2022-10-26 17:10:50 +0400

    Latest updates

---
+++ myapp/models.py
@@ -0,0 +1,20 @@
+from re import T
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=500)
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    
+    
+    def __str__(self) -> str:
+        return self.title
+ 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+ 
+    
---
+++ myapp/views.py
@@ -0,0 +1,92 @@
+from http.client import HTTPS_PORT
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from . models import Post
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+
+posts = [{
+    'author' : 'Nijat',
+    'title' : 'Blog Post 1',
+    'content' : 'First post content',
+    'date_posted': 'August 27, 2018'
+},
+         {
+    'author' : 'Jane Doe',
+    'title' : 'Blog Post 2',
+    'content' : 'Second post content',
+    'date_posted': 'August 28, 2018'
+         }
+]
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'myapp/user_post.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
